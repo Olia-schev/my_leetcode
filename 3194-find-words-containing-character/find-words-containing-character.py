169a4588cd269d67169b7bc4bb2b2adb3b93acf6
Ольга <144008565+Olia-schev@users.noreply.github.com>
2024-09-01 15:54:35 +0300

    Time: 54 ms (87.02%) | Memory: 16.4 MB (83.87%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.py
+++ 3194-find-words-containing-character/find-words-containing-character.py
@@ -2,7 +2,7 @@ class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         index_list = []
         for i in range(len(words)):
-            if words[i].__contains__(x):
+            if x in words[i]:
                 index_list.append(i)
         return(index_list)
 
