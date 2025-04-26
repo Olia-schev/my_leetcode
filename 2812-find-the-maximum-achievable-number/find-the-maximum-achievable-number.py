8fff5c00a8a2ecb3f1b290d22b22e178bf244914
Ольга <144008565+Olia-schev@users.noreply.github.com>
2025-04-26 21:42:20 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.71%) - LeetSync

--- 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def theMaximumAchievableX(self, num, t):
-        ans = []
-        for i in [0,2,-2]:
-            ans.append(num+i*t)
-        return max(ans)
+        return num+2*t
         
