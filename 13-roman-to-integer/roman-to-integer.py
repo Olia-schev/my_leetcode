bcb993592150faaafb54721fb177e634012f055f
Ольга <144008565+Olia-schev@users.noreply.github.com>
2026-01-02 12:41:30 +0300

    Time: 11 ms (16.43%) | Memory: 17.3 MB (92.93%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict_num = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        exceptions = {'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        sum = 0
+        exc_letter = []
+
+        for k in exceptions.keys():
+            try:
+                sum += exceptions[re.findall(k,s)[0]]
+                exc_letter.append(k)
+            except: 
+                continue
+
+        s_new = s
+        for l in exc_letter:
+            s_new = s_new .replace(l,'')
+
+        for i in s_new:
+            sum += dict_num[i]
+        return sum
